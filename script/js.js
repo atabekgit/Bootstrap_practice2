7cdd3ad60d43c487a16e23409c6e8d5df0f4ddcf
atabek <atabekgithub23@gmail.com>
2022-05-31 16:52:34 +0600

    map add

---
+++ script/js.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXRhYmVrMjMiLCJhIjoiY2wzdTA0MHRyMGUxMTNubnYycmE1OTEyaSJ9.m4Azjt3nOQltBpvC_3e9Xg';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.060982,42.35725],
+    zoom:18,
+});
